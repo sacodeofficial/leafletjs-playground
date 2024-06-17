9b0a6d84d7ceacc0057d7a9070f05ee67337ae8c
Samuel Bosawer <zam.bosawer@gmail.com>
2024-06-17 18:11:35 +0900

    Feat: data wilayah yang telah di bagi di distrik abepura

---
+++ assets/geojson/abepura/wilayahAbepura01.js
@@ -0,0 +1,40 @@
+var geoAbepura1 = {
+    "type": "FeatureCollection",
+    "nama": "wilayahAbepura01.js",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "kode_kec": 9471020,
+                "kode_kk": 9471,
+                "nama": "Wilayah Abepura 01",
+                "kode_usaha_distrik": "abepura"
+            },
+            "coordinates": [
+                [
+                  [
+                    140.66854440248426,
+                    -2.5954700153831567
+                  ],
+                  [
+                    140.66854440248426,
+                    -2.601262744948855
+                  ],
+                  [
+                    140.67807954733257,
+                    -2.601262744948855
+                  ],
+                  [
+                    140.67807954733257,
+                    -2.5954700153831567
+                  ],
+                  [
+                    140.66854440248426,
+                    -2.5954700153831567
+                  ]
+                ]
+              ],
+              "type": "Polygon"
+        }
+    ]
+}
